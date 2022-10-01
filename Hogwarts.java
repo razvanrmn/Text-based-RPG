fae2e926186d16c8db5271a185cbd1a6d2bdddbd
Tera <98484095+razvanrmn@users.noreply.github.com>
2022-10-01 11:00:31 +0300

    Update Hogwarts.java

--- Hogwarts.java
+++ Hogwarts.java
@@ -1,8 +1,11 @@
-public class Hogwarts {
+public class Hogwarts
+{
     private String name;
     private String house;
     private String trait;
     private double galleon;
+
+    // Numele o sa fie introdus de la tasatura, sunt 4 Case predefinite, 4 Atribute tot de la tastatura, galleons sunt monedele din H.P deci se refera la bani.
 
     public String getName() {
         return name;
@@ -11,48 +14,44 @@ public class Hogwarts {
     public String getHouse() {
         return house;
     }
-
     public String getTrait() {
         return trait;
     }
-
     public double getGalleon() {
         return galleon;
     }
-
     public void setName(String newName) {
         name = newName;
     }
-
     public void setHouse(String newHouse) {
         house = newHouse;
     }
-
     public void setTrait(String newTrait) {
         trait = newTrait;
     }
-
     public void setGalleon(double newGalleon) {
         galleon = newGalleon;
     }
-
+    //Get si set, nimic special aici
     public Hogwarts(String name) {
         this.name = name;
     }
+    // Constructorul
 
     public void sortingHat(String trait) {
-        if (trait.equals("Bravery")) {
-            house = "GRIFFINDOR!";
-        } else if (trait.equals("Intelligence")) {
-            house = "RAVENCLAW!";
-        } else if (trait.equals("Empathy")) {
-            house = "HUFFLEPUFF!";
-        } else if (trait.equals("Ambition")) {
-            house = "SLYTHERIN!";
+        if (trait.equals("BRAVERY")) {
+            house = "\nGRIFFINDOR!";
+        } else if (trait.equals("INTELLIGENCE")) {
+            house = "\nRAVENCLAW!";
+        } else if (trait.equals("EMPATHY")) {
+            house = "\nHUFFLEPUFF!";
+        } else if (trait.equals("AMBITION")) {
+            house = "\nSLYTHERIN!";
         } else {
             System.out.println("Think again");
         }
     }
+    // Am creeat functia sorting hat care te distribuie intr-o anumita clasa in functie de ce trait te defineste
 
     public String toString() {
         StringBuffer string = new StringBuffer();
@@ -63,27 +62,27 @@ public class Hogwarts {
         string.append("\nYours sincerely,\nMinerva McGonagall\nDeputy Headmistress\n");
         return string.toString();
     }
-
+    // Am folosit toString ca sa afisez scrisoarea pe care o primesti de la Dumbledore, insotita de numele introdus de tine.
     public void letter(String name, String answer) {
-        if (answer.equals("Yes"))
+        if (answer.equals("Y"))
             System.out.println(toString());
         else
             System.exit(0);
     }
-
+    // Acest mesaj va aparea dupa ce iti introduci numele, daca vrei sa aplici la Hogwarts aplicatia continua sa ruleze else se opreste.
     public void galleonCheck() {
         System.out.println("Your available balance is " + galleon);
     }
-
+    // Verifica cati bani ai in cont.
     public void galleonDeposit(double amount) {
         galleon += amount;
     }
-
+    // Depunere in cont.
     public void purchase(double amount)
     {
         galleon -= amount;
     }
-
+    // Se foloseste cand doresti sa achizitionezi ceva.
     public boolean expelliarmus(boolean expelliarmus)
     {
         if (expelliarmus == true) {
@@ -139,5 +138,6 @@ public class Hogwarts {
             return false;
         }
     }
+    // Aici am facut cate o functie pentru fiecare Spell care o sa aiba o sansa random de succes.
 }
 
